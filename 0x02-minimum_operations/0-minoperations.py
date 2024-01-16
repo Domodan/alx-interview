bbcc64267b3530f2031a0482cffa6629d11887b1
domodan <ongomdaniel9@gmail.com>
2024-01-16 07:05:03 +0000

    written a method that calculates the fewest number of operations needed to result in exactly n H characters in the file

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -7,7 +7,8 @@ Module: Minimum Operations
 
 def minOperations(n):
     """
-    A method that calculates the fewest number of operations needed to result in exactly n H characters in the file
+    A method that calculates the fewest number of operations needed to
+    result in exactly n H characters in the file
     If n is impossible to achieve, returns 0
     """
     if not isinstance(n, int):
