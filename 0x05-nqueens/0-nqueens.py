12388305b98c2bd40ed7a969f9eba3eab1276094
Domodan <ongomdaniel9@gmail.com>
2024-02-10 10:38:09 +0000

    written a program that solves the N queens problem

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,22 +1,18 @@
 #!/usr/bin/python3
-"""N queens solution finder module.
+"""
+    Module: Program that solves N Queens Problem
 """
 import sys
 
 
 solutions = []
-"""The list of possible solutions to the N queens problem.
-"""
 n = 0
-"""The size of the chessboard.
-"""
 pos = None
-"""The list of possible positions on the chessboard.
-"""
 
 
 def get_input():
-    """Retrieves and validates this program's argument.
+    """
+    Retrieves and validates this program's argument.
 
     Returns:
         int: The size of the chessboard.
@@ -38,7 +34,8 @@ def get_input():
 
 
 def is_attacking(pos0, pos1):
-    """Checks if the positions of two queens are in an attacking mode.
+    """
+    Checks if the positions of two queens are in an attacking mode.
 
     Args:
         pos0 (list or tuple): The first queen's position.
@@ -53,7 +50,8 @@ def is_attacking(pos0, pos1):
 
 
 def group_exists(group):
-    """Checks if a group exists in the list of solutions.
+    """
+    Checks if a group exists in the list of solutions.
 
     Args:
         group (list of integers): A group of possible positions.
@@ -74,7 +72,8 @@ def group_exists(group):
 
 
 def build_solution(row, group):
-    """Builds a solution for the n queens problem.
+    """
+    Builds a solution for the n queens problem.
 
     Args:
         row (int): The current row in the chessboard.
@@ -98,7 +97,8 @@ def build_solution(row, group):
 
 
 def get_solutions():
-    """Gets the solutions for the given chessboard size.
+    """
+        Gets the solutions for the given chessboard size.
     """
     global pos, n
     pos = list(map(lambda x: [x // n, x % n], range(n ** 2)))
